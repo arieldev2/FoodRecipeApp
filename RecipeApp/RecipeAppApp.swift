0edbf4ccd6745766b894477bf18c15688a4b87c0
Ariel Ortiz <arielortiz.dev@gmail.com>
2025-08-27 21:38:18 -0700

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Ariel Ortiz on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
